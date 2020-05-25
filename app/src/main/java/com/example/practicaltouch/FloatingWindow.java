6451a3fa1040a63c3228c3d31506793eaf0c42f4
duckmoon99 <e0407816@u.nus.edu>
2020-05-26 01:11:24 +0800

    App Tray update
    Add AppTray class
    Add BubbleImageView class
    Update image asset
    Update behavior of floating window
    Update behavior of launch button

--- app/src/main/java/com/example/practicaltouch/FloatingWindow.java
+++ app/src/main/java/com/example/practicaltouch/FloatingWindow.java
@@ -3,26 +3,24 @@ package com.example.practicaltouch;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.os.Build;
 import android.os.IBinder;
-import android.os.Parcelable;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class FloatingWindow extends Service {
     final String tag = "floatingWindow";
@@ -31,13 +29,16 @@ public class FloatingWindow extends Service {
     WindowManager windowManager;
     LinearLayout frontLayer;
     LinearLayout backLayer;
+    LinearLayout trayLayer;
+    LinearLayout appTray;
+    HorizontalScrollView appTrayContainer;
+
     Point screenSize = new Point();
     ArrayList<String> appList;
-    ImageView openapp;
+    BubbleImageView openapp;
     ImageView crossIcon;
     WindowManager.LayoutParams params;
     private PackageManager packageManager;
-
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
@@ -45,15 +46,8 @@ public class FloatingWindow extends Service {
 
         appList = intent.getStringArrayListExtra("com.example.practicaltouch.addedApp");
         assert appList != null;
-        openapp = new ImageView(this);
-        try {
-            openapp.setImageDrawable(packageManager.getApplicationIcon(appList.get(0)));
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        ViewGroup.LayoutParams butnparams = new ViewGroup.LayoutParams(
-                150,150);
-        openapp.setLayoutParams(butnparams);
+        openapp = new BubbleImageView(this);
+
         frontLayer.addView(openapp);
         windowManager.addView(frontLayer, params);
 
@@ -63,6 +57,7 @@ public class FloatingWindow extends Service {
             double y;
             double px;
             double py;
+            boolean moved = false;
 
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -75,20 +70,25 @@ public class FloatingWindow extends Service {
                         backLayer.addView(crossIcon);
                         break;
                     case MotionEvent.ACTION_UP:
-                        backLayer.removeView(crossIcon);
-                        windowManager.updateViewLayout(frontLayer,updatepar);
-                        if(Math.abs(screenSize.y - updatepar.y) <= 450 && Math.abs(updatepar.x - screenSize.x/2) <= 300) {
-                            stopSelf();
-                        } else {
-                            if (updatepar.x >= screenSize.x / 2) {
-                                updatepar.x = screenSize.x - 170;
+                        if (moved) {
+                            backLayer.removeView(crossIcon);
+                            windowManager.updateViewLayout(frontLayer, updatepar);
+                            if (Math.abs(screenSize.y - updatepar.y) <= 350 && Math.abs(updatepar.x - screenSize.x / 2) <= 300) {
+                                stopSelf();
                             } else {
-                                updatepar.x = 16;
+                                if (updatepar.x >= screenSize.x / 2) {
+                                    updatepar.x = screenSize.x - 230;
+                                } else {
+                                    updatepar.x = 0;
+                                }
+                                windowManager.updateViewLayout(frontLayer, updatepar);
                             }
-                            windowManager.updateViewLayout(frontLayer,updatepar);
+                        } else {
+                            openapp.performClick();
                         }
                         break;
                     case MotionEvent.ACTION_MOVE:
+                        moved = true;
                         updatepar.x = (int) (x-(motionEvent.getRawX()-px));
                         updatepar.y = (int) (y+(motionEvent.getRawY()-py));
                         windowManager.updateViewLayout(frontLayer,updatepar);
@@ -99,27 +99,22 @@ public class FloatingWindow extends Service {
             }
         });
 
-
-        openapp.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view){
-                return true;
-            }
-        });
-
         openapp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                params.x = 16;
-                params.y = 16;
-                windowManager.updateViewLayout(frontLayer,params);
-                Intent startApp = packageManager.getLaunchIntentForPackage(appList.get(0));
-                startActivity(startApp);
-//                Intent home = new Intent(FloatingWindow.this, MainActivity.class);
-//                home.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                startActivity(home);
+                if (openapp.isOpen()) {
+                    appTrayContainer.removeView(appTray);
+                } else {
+                    appTrayContainer.addView(appTray);
+                }
+                openapp.toggle();
             }
         });
+
+        appTray = new AppTray(this, appList, packageManager);
+        appTrayContainer = new HorizontalScrollView(this);
+
+        trayLayer.addView(appTrayContainer);
 
         return START_REDELIVER_INTENT;
     }
@@ -131,12 +126,19 @@ public class FloatingWindow extends Service {
     }
 
     @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        windowManager.getDefaultDisplay().getSize(screenSize);
+    }
+
+    @Override
     public void onCreate() {
         super.onCreate();
 
         packageManager = getPackageManager();
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         assert windowManager != null;
+
         windowManager.getDefaultDisplay().getSize(screenSize);
         frontLayer = new LinearLayout(this);
         frontLayer.setBackgroundColor(Color.TRANSPARENT);
@@ -151,16 +153,12 @@ public class FloatingWindow extends Service {
                         : WindowManager.LayoutParams.TYPE_PHONE),
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);
-
         params.gravity = Gravity.END | Gravity.TOP;
-        params.x = 16;
-        params.y = 16;
 
         backLayer = new LinearLayout(this);
         backLayer.setBackgroundColor(Color.TRANSPARENT);
         backLayer.setLayoutParams(layoutParams);
-
-
+        
         final WindowManager.LayoutParams crossParam = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,
                 (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
@@ -173,10 +171,23 @@ public class FloatingWindow extends Service {
         crossIcon.setImageResource(R.mipmap.floating_cross_foreground);
         crossIcon.setLayoutParams(new ViewGroup.LayoutParams(200, 200));
         crossParam.gravity = Gravity.BOTTOM;
-        crossParam.x = 16;
-        crossParam.y = 16;
 
-        windowManager.addView(backLayer,crossParam);
+        windowManager.addView(backLayer, crossParam);
+
+        trayLayer = new LinearLayout(this);
+        trayLayer.setBackgroundColor(Color.TRANSPARENT);
+        trayLayer.setLayoutParams(layoutParams);
+
+        final WindowManager.LayoutParams trayParam = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,
+                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+                        ? WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                        : WindowManager.LayoutParams.TYPE_PHONE),
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        trayParam.gravity = Gravity.TOP | Gravity.END;
+
+        windowManager.addView(trayLayer, trayParam);
     }
 
     @Override
@@ -185,6 +196,7 @@ public class FloatingWindow extends Service {
         started = false;
         windowManager.removeView(backLayer);
         windowManager.removeView(frontLayer);
+        windowManager.removeView(trayLayer);
     }
 
     public static boolean hasStarted() {
