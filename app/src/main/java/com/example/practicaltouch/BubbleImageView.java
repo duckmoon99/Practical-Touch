c70bf07028c22a67e81448c8e5cf1b04f97c1d95
duckmoon99 <e0407816@u.nus.edu>
2020-07-19 10:27:14 +0800

    Add transparency to dock

--- app/src/main/java/com/example/practicaltouch/BubbleImageView.java
+++ app/src/main/java/com/example/practicaltouch/BubbleImageView.java
@@ -17,13 +17,11 @@ public class BubbleImageView extends ImageView {
         open = false;
         setImageResource(R.drawable.logo);
         setLayoutParams(new ViewGroup.LayoutParams(150, 150));
-        setOnLongClickListener(new OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                context.startActivity(new Intent(context, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-                Toast.makeText(context, "Switching App Set", Toast.LENGTH_SHORT).show();
-                return true;
-            }
+        setAlpha((float) 0.5);
+        setOnLongClickListener(v -> {
+            context.startActivity(new Intent(context, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            Toast.makeText(context, "Switching App Set", Toast.LENGTH_SHORT).show();
+            return true;
         });
     }
 
